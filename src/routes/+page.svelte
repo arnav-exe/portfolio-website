368ebfd6ee7d6b6e7fe54af3351f8097161bb171
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-05-24 14:42:50 +0100

    basic homepage and blog setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+    import { LightSwitch } from "@skeletonlabs/skeleton"
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+
+<div class="p-8 space-y-8">
+    <div class="flex justify-between items-center h-full mx-auto">
+    <LightSwitch />
+
+    <!-- REMAKE AJ LOGO PROPERLY -->
+	<a href="/"><img src="../logo.svg" class="w-8" alt=""></a>
+
+	<a class="justify-end" href="/blog">BLOG -&gt;</a>
 	</div>
+
+    <div class="flex justify-center items-center h-screen flex-col mx-auto max-w-[90%]">
+
+		<h1 class="h1 -translate-x-10 asdf bg-gradient-to-br from-neutral-950 to-neutral-200 bg-clip-text text-transparent box-decoration-clone">ARNAV</h1>
+		<h1 class="h1 translate-x-10 asdf bg-gradient-to-br from-neutral-200 to-neutral-950 bg-clip-text text-transparent box-decoration-clone">JAGETIA</h1>
+    </div>
 </div>
+
+<style>
+	.asdf  {
+		font-size: 10rem;
+	}
+</style>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { LightSwitch } from "@skeletonlabs/skeleton"
+</script>
+
+
+<div class="p-8 space-y-8">
+    <div class="flex justify-between items-center h-full mx-auto">
+    <LightSwitch />
+
+    <!-- REMAKE AJ LOGO PROPERLY -->
+	<a href="/"><img src="../logo.svg" class="w-8" alt=""></a>
+
+	<a class="justify-end" href="/">&lt;- HOME</a>
+	</div>
+
+    <div class="flex justify-center items-center h-full flex-col mx-auto max-w-[90%]">
+
+        <h1 class="h1 m-4">BLOG</h1>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati delectus possimus temporibus voluptatibus explicabo ex neque atque? Inventore aspernatur non, soluta fugiat quo in alias, provident, quia fugit consequatur id.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati delectus possimus temporibus voluptatibus explicabo ex neque atque? Inventore aspernatur non, soluta fugiat quo in alias, provident, quia fugit consequatur id.</p>
+
+    </div>					
+</div>
