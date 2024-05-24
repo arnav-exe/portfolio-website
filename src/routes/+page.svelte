f94428c167f427af087742569facf230521cf2c4
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-05-25 00:56:20 +0100

    scaffolded out remaineder of content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,18 +10,57 @@
     <div class="flex justify-between items-center h-full mx-auto">
     <LightSwitch />
 
-	<a class="justify-end" href="/blog">BLOG -&gt;</a>
+	<a class="justify-end" href="/blog">BLOG</a>
 	</div>
 
-    <div class="flex justify-center items-center h-screen flex-col mx-auto max-w-[90%]">
+    <div class="flex justify-center items-center h-full flex-col mx-auto max-w-[90%]">
+		<!-- LANDING PAGE -->
+		<div>
+			<h1 class="h1 -translate-x-24 title">ARNAV</h1>
+			<h1 class="h1 translate-x-24 title">JAGETIA</h1>
+		</div>
 
-		<h1 class="h1 -translate-x-24 asdf">ARNAV</h1>
-		<h1 class="h1 translate-x-24 asdf">JAGETIA</h1>
+		<!-- ABOUT ME -->
+		<div>
+			<h1 class="h1">ABOUT ME</h1>
+			<p>Hi, I'm a recent Computer Systems Engineering graduate from QMUL. As a full-stack engineer, I craft sleek, powerful apps with Svelte, JavaScript, Node.js, and Tailwind. I also specialize in machine learning and data analysis using PyTorch, Hadoop, and Spark. Not to mention additional competencies in C, VHDL, and Java</p>
+			<p>I also excel in architecting resilient and scalable cloud-based solutions using AWS, backed by two AWS certifications: AWS Associate Developer and AWS Cloud Practitioner.</p>
+		</div>
+
+		<!-- SKILLS -->
+		<div>
+			<h1 class="h1">SKILLS</h1>
+			<!-- single row of logos of all languages/frameworks I am proficient in -->
+		</div>
+
+		<!-- PROJECTS -->
+		<div>
+			<h1 class="h1">PROJECTS</h1>
+		</div>
+
+		<!-- EXPERIENCE -->
+		<div>
+			<h1 class="h1">EXPERIENCE</h1>
+		</div>
+
+		<!-- CONTACT -->
+		<div>
+			<h1 class="h1">CONTACT ME</h1>
+		</div>
+
+		<!-- FOOTER -->
+		<div>
+			<!-- things to put in footer: -->
+			<!-- all my socials (github, linkedin, email?) -->
+			<!-- made with :heart: using svelte, SkeletonUI and Tailwind -->
+			<!-- copyright @me? -->
+			<!-- funny quote? -->
+		</div>
     </div>
 </div>
 
 <style>
-	.asdf  {
+	.title  {
 		font-size: 15rem;
 	}
 </style>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,12 @@
 <script>
     import { LightSwitch } from "@skeletonlabs/skeleton"
+
+    import Cursor from "../../lib/Cursor.svelte";
 </script>
 
+
+
+<Cursor />
 
 <div class="p-8 space-y-8">
     <div class="flex justify-between items-center h-full mx-auto">
@@ -10,7 +15,7 @@
         <a href="/">
             <svg class="w-8 fill-surface-500 dark:fill-primary-500" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3000 1879.55"><polygon points="1085.04 0.2 957.42 221.26 957.3 221.05 1084.92 0 1085.04 0.2"/><polygon points="2042.46 1658.5 1914.84 1879.55 1914.63 1879.19 2042.25 1658.14 2042.46 1658.5"/><polygon points="3000 0.58 2872.38 221.63 2871.63 221.63 2042.25 1658.14 1914.63 1879.19 1787.01 1658.14 1085.05 442.31 255.25 1879.55 0 1879.55 957.42 221.26 1085.04 0.2 1212.66 221.26 1914.63 1437.1 2616.38 221.63 1978.65 221.63 1978.65 0.58 3000 0.58"/><polygon points="1212.79 221.05 1212.66 221.26 1085.04 0.2 1085.16 0 1212.79 221.05"/><polygon points="1914.63 1879.19 1914.42 1879.55 1786.8 1658.5 1787.01 1658.14 1914.63 1879.19"/><polygon points="2744.67 0.58 2744 0.58 2744.34 0 2744.67 0.58"/></svg>
         </a>
-        <a class="justify-end" href="/">&lt;- HOME</a>        
+        <a class="justify-end" href="/">HOME</a>        
 	</div>
 
     <div class="flex justify-center items-center h-full flex-col mx-auto max-w-[90%]">
