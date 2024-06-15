7265531893b71582c16d47af16f6b38c4bb1e83f
Arnav Jagetia <arnav.jagetia03@gmail.com>
2024-06-15 23:24:16 +0100

    TIMELINE IS NO LONGER COOKED (keeping legacy code just in case tho)

--- src/lib/Timeline/Timeline.svelte
+++ src/lib/Timeline/Timeline.svelte
@@ -1,3 +1,3 @@
-<ol class="relative border-l">
-	<slot class="bg-surface-500 dark:bg-primary-500" />
+<ol class="relative border-s border-l-surface-500 dark:border-l-primary-500">
+	<slot />
 </ol>
--- src/lib/Timeline/TimelineItem.svelte
+++ src/lib/Timeline/TimelineItem.svelte
@@ -11,19 +11,19 @@
 	<span class="absolute flex items-center justify-center w-3 h-3 bg-surface-500 rounded-full -left-1.5 dark:bg-primary-500"></span>
 	
 	<!-- start & end date -->
-	<p class="block my-4 text-tertiary-700 dark:text-secondary-700">{startDate} - {endDate}</p>
+	<p class="block my-4 text-surface-400 dark:text-secondary-700">{startDate} - {endDate}</p>
 	
 	<!-- Title -->
-	<h3 class="flex items-center mb-1 space-x-2 -translate-y-2">
-		<span class="text-surface-500 dark:text-primary-500">{title}</span>
+	<h6 class="flex items-center mb-1 space-x-2 -translate-y-2">
+		<span class="h6 text-surface-500 dark:text-primary-500">{title}</span>
 		<!-- Badge -->
 		{#if badgeText}
 			<span class="badge bg-secondary-500 dark:bg-tertiary-500">{badgeText}</span>
 		{/if}
-	</h3>
+	</h6>
 	
 	<!-- Description -->
-	<p class="mb-4 opacity-75 text-surface-700 dark:text-primary-700">{description}</p>
+	<p class="mb-4 opacity-75 text-surface-400 dark:text-primary-700">{description}</p>
 	
 	<!-- Slot: Default -->
 	{#if $$slots.default}
