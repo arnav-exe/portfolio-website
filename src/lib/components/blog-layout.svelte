acf952c2be94887582dc92ee2c66871ecee2b0e8
arnav-exe <arnav.jagetia03@gmail.com>
2026-03-09 19:08:24 -0400

    removed double underline

--- src/lib/components/blog-layout.svelte
+++ src/lib/components/blog-layout.svelte
@@ -21,7 +21,7 @@
 		<!-- Back button -->
 		<a
 			href="/blog"
-			class="inline-flex items-center mb-8 pb-0.5 border-b border-transparent text-surface-500 hover:text-surface-400 dark:text-primary-500 dark:hover:text-primary-400 hover:underline underline-offset-4 hover:border-current transition-colors"
+			class="inline-flex items-center mb-8 pb-0.5 border-b border-transparent text-surface-500 hover:text-surface-400 dark:text-primary-500 dark:hover:text-primary-400 underline-offset-4 hover:border-current transition-colors"
 		>
 			<svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 				<path
