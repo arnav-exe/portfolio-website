89618b3d0f7227a7b0d75244f8b5eaed3190eb7a
arnav-exe <avij4263@gmail.com>
2026-09-01 19:13:39 +0100

    format root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,8 @@
 	});
 
 	// logos for footer
-	import GitHub from "../lib/icons/GitHub.svelte";
-	import Linkedin from "../lib/icons/Linkedin.svelte";
+	import GitHub from '../lib/icons/GitHub.svelte';
+	import Linkedin from '../lib/icons/Linkedin.svelte';
 
 	// fetch current year
 	let year = new Date().getFullYear();
@@ -31,10 +31,22 @@
 <div class="w-full h-56 bg-secondary-500 dark:bg-surface-500">
 	<div class="flex justify-center items-center flex-col">
 		<div class="flex justify-center flex-row">
-			<a class="w-10 mx-4 my-8 dark:fill-primary-500 fill-logosurface dark:hover:fill-primary-400 hover:fill-surface-400" href="https://www.github.com/arnav-exe/"><GitHub /></a>
-			<a class="w-10 mx-4 my-8 dark:fill-primary-500 fill-logosurface dark:hover:fill-primary-400 hover:fill-surface-400" href="https://www.linkedin.com/in/arnav-jagetia/"><Linkedin /></a>
+			<a
+				class="w-10 mx-4 my-8 dark:fill-primary-500 fill-logosurface dark:hover:fill-primary-400 hover:fill-surface-400"
+				href="https://www.github.com/arnav-exe/"><GitHub /></a
+			>
+			<a
+				class="w-10 mx-4 my-8 dark:fill-primary-500 fill-logosurface dark:hover:fill-primary-400 hover:fill-surface-400"
+				href="https://www.linkedin.com/in/arnav-jagetia/"><Linkedin /></a
+			>
 		</div>
-		<p class="mb-8 text-center mx-16">Forged in the heart of a dying star using <a class="underline" href="https://kit.svelte.dev/">SvelteKit</a>, <a class="underline" href="https://tailwindcss.com/">Tailwind</a>, <a class="underline " href="https://www.skeleton.dev/">SkeletonUI</a>, and <a class="underline " href="https://gsap.com/">gsap</a></p>
+		<p class="mb-8 text-center mx-16">
+			Forged in the heart of a dying star using <a class="underline" href="https://kit.svelte.dev/"
+				>SvelteKit</a
+			>, <a class="underline" href="https://tailwindcss.com/">Tailwind</a>,
+			<a class="underline" href="https://www.skeleton.dev/">SkeletonUI</a>, and
+			<a class="underline" href="https://gsap.com/">gsap</a>
+		</p>
 		<p class="mb-8">© Arnav Jagetia {year}</p>
 	</div>
 </div>
