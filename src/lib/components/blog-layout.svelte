8798d0e7149792a2b7736b7652595aba430c8652
arnav <avij4263@gmail.com>
2026-09-02 14:28:04 +0100

    added lucide

--- src/lib/components/blog-layout.svelte
+++ src/lib/components/blog-layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { MoveLeft } from 'lucide-svelte';
+
 	export let title = 'Blog Post';
 	export let description = '';
 	export let date = '';
@@ -39,14 +41,7 @@
 			href="/blog"
 			class="inline-flex items-center mb-8 pb-0.5 border-b border-transparent text-surface-500 hover:text-surface-400 dark:text-primary-500 dark:hover:text-primary-400 underline-offset-4 hover:border-current transition-colors"
 		>
-			<svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M10 19l-7-7m0 0l7-7m-7 7h18"
-				/>
-			</svg>
+			<MoveLeft class="w-5 h-5 mr-2" />
 			Back to Blog
 		</a>
 
